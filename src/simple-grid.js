9392ee857fca6f51a1646457a3d8ca0cd0aa3a64
Noam Lewis <lenoam+BDB@gmail.com>
2013-12-24 11:59:00 -0500

    Added feature: double-click on cell to enter edit mode

--- src/simple-grid.js
+++ src/simple-grid.js
@@ -11,35 +11,54 @@
                 },
 
                 link: function (scope, elem, attrs) {
-                    
+
                     function isEditable(editable) {
-                        $log.debug('isEditable');
+                        //$log.debug('isEditable');
                         if (angular.isUndefined(editable)) {
                             return true; // editable by default
                         }
                         return editable || false;
                     }
-                    
+
+                    function setFocusedCell(event, targetRowIndex, colIndex) {
+                        var elem = null, elems;
+                        if (null !== targetRowIndex) {
+                            elem = document.getElementById(scope.formName(targetRowIndex));
+                        }
+                        if (!elem) {
+                            return;
+                        }
+                        if (event) {
+                            event.preventDefault();
+                        }
+                        elems = elem.getElementsByClassName('sg-column-' + colIndex, angular.element(elem));
+                        if (elems.length) {
+                            $timeout(function () {
+                                elems[0].focus();
+                            });
+                        }
+                    }
+
                     function initialize() {
                         scope.gridNum = gridNum;
                         gridNum += 1;
-                        
+
                         scope.page = [];
                         scope.selectedRow = null;
                         scope.focusedRow = null;
-                        
+
                         scope.$watchCollection('simpleGrid.getData()', function (newVal) {
                             scope.data = newVal;
                             scope.updatePage();
                         });
-                        
+
                         scope.$watch('simpleGrid.options.pageSize', scope.updatePage);
                         scope.$watch('simpleGrid.options.pageNum', scope.updatePage);
-                        
+
                         scope.$watch('simpleGrid.options.editable', function (editable) {
                             scope.gridIsEditable = isEditable(editable);
                         });
-                        
+
                         scope.$watch('simpleGrid.options.columns', function (newVal) {
                             angular.forEach(newVal, function (column) {
                                 if (column.inputType === 'select') {
@@ -48,9 +67,9 @@
                                 column.$title = column.title || scope.capitalize(column.field);
                             });
                         }, true);
-                        
+
                     }
-                    
+
                     scope.updatePage = function () {
                         var i,
                             pageSize,
@@ -62,15 +81,15 @@
                         pageSize = scope.simpleGrid.options.pageSize || scope.data.length;
                         pageStart = (scope.simpleGrid.options.pageNum || 0) * pageSize;
                         for (i = pageStart;
-                                i < Math.min(pageStart + pageSize,
-                                             scope.data.length);
-                                i += 1) {
+                             i < Math.min(pageStart + pageSize,
+                                 scope.data.length);
+                             i += 1) {
                             scope.page.push(scope.data[i]);
                         }
                     };
 
                     scope.capitalize = function (str) {
-                        $log.debug('capitalize');
+                        //$log.debug('capitalize');
                         if (!str) {
                             return str;
                         }
@@ -94,7 +113,7 @@
                     };
 
                     scope.isInvalid = function (rowIndex) {
-                        $log.debug('isInvalid', rowIndex);
+                        //$log.debug('isInvalid', rowIndex);
                         var formCtrl = scope.$eval(scope.formName(rowIndex));
                         return formCtrl.$error;
                     };
@@ -103,30 +122,18 @@
                         var elem = null, elems,
                             targetRowIndex = null;
                         switch (event.keyCode) {
-                        case 40: //down
-                            targetRowIndex = rowIndex + 1;
-                            break;
-                        case 38: //up
-                            targetRowIndex = rowIndex - 1;
-                            break;
+                            case 40: //down
+                                targetRowIndex = rowIndex + 1;
+                                break;
+                            case 38: //up
+                                targetRowIndex = rowIndex - 1;
+                                break;
                         }
-                        if (null !== targetRowIndex) {
-                            elem = document.getElementById(scope.formName(targetRowIndex));
-                        }
-                        if (!elem) {
-                            return;
-                        }
-                        event.preventDefault();
-                        elems = elem.getElementsByClassName('sg-column-' + colIndex, angular.element(elem));
-                        if (elems.length) {
-                            $timeout(function () {
-                                elems[0].focus();
-                            });
-                        }
+                        setFocusedCell(event, targetRowIndex, colIndex);
                     };
 
                     scope.getCellText = function (row, column) {
-                        $log.debug('getCellText');//, row, column);
+                        //$log.debug('getCellText');//, row, column);
                         var cellValue = row[column.field];
                         if (column.inputType === 'select') {
                             return scope.getOptionTitleByValue(column.options, cellValue);
@@ -138,7 +145,7 @@
                     };
 
                     scope.getOptionTitleByValue = function (options, value) {
-                        $log.debug('getOptionTitleByValue');//, options, value);
+                        //$log.debug('getOptionTitleByValue');//, options, value);
                         // TODO: Highly ineffecient.
                         // TODO: Array.prototype.filter is not compatible with older browsers
                         var filteredOptions = options.filter(function (option) {
@@ -161,7 +168,7 @@
                     };
 
                     scope.selectRow = function (row) {
-                        $log.debug('selectRow', row);
+                        //$log.debug('selectRow', row);
                         if (!scope.simpleGrid.options.allowMultiSelect) {
                             if (scope.selectedRow && scope.selectedRow.$selected) {
                                 delete scope.selectedRow.$selected;
@@ -180,7 +187,7 @@
                     };
 
                     scope.setFocusedRow = function (row) {
-                        $log.debug('setFocusedRow', row);
+                        //$log.debug('setFocusedRow', row);
                         if (scope.focusedRow === row) {
                             return;
                         }
@@ -208,7 +215,7 @@
                     };
 
                     scope.cellFocused = function (row, column) {
-                        $log.debug('cellFocused', row, column);
+                        //$log.debug('cellFocused', row, column);
                         scope.setFocusedRow(row);
                         if (!scope.simpleGrid.options.allowMultiSelect) {
                             scope.selectRow(row);
@@ -217,9 +224,16 @@
                             scope.simpleGrid.options.cellFocused(row, column);
                         }
                     };
-                    
+
+                    scope.startEditingCell = function (event, rowIndex, columnIndex, row, column) {
+                        row.$editable = true;
+                        $timeout(function() {
+                            setFocusedCell(null, rowIndex, columnIndex);
+                        });
+                    };
+
                     scope.getOptions = function (options) {
-                        $log.debug('getOptions');//, options);
+                        //$log.debug('getOptions');//, options);
                         if (options.length && angular.isString(options[0])) {
                             // TODO: Not compatible with old browsers
                             return options.map(function (val) { return { value: val, title: scope.capitalize(val) }; });
